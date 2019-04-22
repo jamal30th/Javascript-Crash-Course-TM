88476a13c72a0c7f3b65da5ca3bb50d60800c78b
Muhammad Jamal Uddin <jamal30th@gmail.com>
2019-04-23 01:07:10 +0600

    java script dom3

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-const items = document.querySelectorAll('.item');
-items.forEach((item) => console.log(item));
-    
+const ul = document.querySelector('.items')
+// ul.remove();
+// ul.lastElementChild.remove();
+// ul.firstElementChild.textContent='Hello'
+ul.children[1].innerHTML = 'Brad'
+ul.children[0].innerText = 'Andrew Russel'
+ul.children[2].textContent = 'mS D'
+ul.lastElementChild.innerHTML = '<h1>Rishab Pant</h1>'
+
+const btn = document.querySelector('.btn');
+btn.style.background = 'red'
