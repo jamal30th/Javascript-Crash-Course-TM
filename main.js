eb4871fd60a0ec21b5bd306730b0d11551743050
Muhammad Jamal Uddin <jamal30th@gmail.com>
2019-04-22 15:28:50 +0600

    class in vanilla javascript

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-x = 5;
-y = 6;
-z = x + y;
-console.log(z);
+class Person{
+    constructor(firstname, lastname, dob) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.dob = dob;
+    }
+    getBirthYear() {
+        return this.dob.getFullYear;
+    }
+    getFullName() {
+        return `${this.firstname} ${this.lastname}`
+    }
+}
+const person1 = new Person('john', 'Doe', '4-3-1980')
+const person2 = new Person('Arohee', 'Jamal', '3-3-2014')
+console.log(person2.getFullName());
+console.log(person1.getBirthYear());
