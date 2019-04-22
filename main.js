7d1532fcfc58924dba2485c72aff2cdde8278960
Muhammad Jamal Uddin <jamal30th@gmail.com>
2019-04-22 19:56:42 +0600

    javascript dom

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-class Person{
-    constructor(firstname, lastname, dob) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.dob = dob;
-    }
-    getBirthYear() {
-        return this.dob.getFullYear;
-    }
-    getFullName() {
-        return `${this.firstname} ${this.lastname}`
-    }
-}
-const person1 = new Person('john', 'Doe', '4-3-1980')
-const person2 = new Person('Arohee', 'Jamal', '3-3-2014')
-console.log(person2.getFullName());
-console.log(person1.getBirthYear());
+const items = document.querySelectorAll('.item');
+items.forEach((item) => console.log(item));
+    
